1090dd347252fd2173ab7ba460b71f356899eed1
dm47021 <dm47021@hotmail.com>
2014-11-01 08:26:22 -0400

    prebuilt-etc.mk massive commit

---
+++ prebuilt/prebuilt-etc.mk
@@ -0,0 +1,137 @@
+# ETC
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/etc/apns-conf.xml:system/etc/apns-conf.xml \
+     $(LOCAL_PATH)/etc/audio_policy.conf:system/etc/audio_policy.conf \
+     $(LOCAL_PATH)/etc/init.qcom.audio.sh:system/etc/init.qcom.audio.sh \
+     $(LOCAL_PATH)/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+     $(LOCAL_PATH)/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
+     $(LOCAL_PATH)/etc/init.qcom.efs.sync.sh:system/etc/init.qcom.efs.sync.sh \
+     $(LOCAL_PATH)/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
+     $(LOCAL_PATH)/etc/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh \
+     $(LOCAL_PATH)/etc/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+     $(LOCAL_PATH)/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+     $(LOCAL_PATH)/etc/init.qcom.post_fs.sh:system/etc/init.qcom.post_fs.sh \
+     $(LOCAL_PATH)/etc/init.qcom.sdio.sh:system/etc/init.qcom.sdio.sh \
+     $(LOCAL_PATH)/etc/init.qcom.thermald_conf.sh:system/etc/init.qcom.thermald_conf.sh \
+     $(LOCAL_PATH)/etc/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh \
+     $(LOCAL_PATH)/etc/media_codecs.xml:system/etc/media_codecs.xml \
+     $(LOCAL_PATH)/etc/media_profiles.xml:system/etc/media_profiles.xml \
+     $(LOCAL_PATH)/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
+     $(LOCAL_PATH)/etc/thermald.conf:system/etc/thermald.conf \
+     $(LOCAL_PATH)/etc/vold.fstab:system/etc/vold.fstab 
+
+# FIRMWARE
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+     $(LOCAL_PATH)/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+     $(LOCAL_PATH)/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+     $(LOCAL_PATH)/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
+     $(LOCAL_PATH)/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+     $(LOCAL_PATH)/etc/firmware/acdb.mbn:system/etc/firmware/acdb.mbn \
+     $(LOCAL_PATH)/etc/firmware/apps.mbn:system/etc/firmware/apps.mbn \
+     $(LOCAL_PATH)/etc/firmware/cyttsp_8064_mtp.hex:system/etc/firmware/cyttsp_8064_mtp.hex \
+     $(LOCAL_PATH)/etc/firmware/cyttsp_8960_cdp.hex:system/etc/firmware/cyttsp_8960_cdp.hex \
+     $(LOCAL_PATH)/etc/firmware/dsp1.mbn:system/etc/firmware/dsp1.mbn \
+     $(LOCAL_PATH)/etc/firmware/dsp2.mbn:system/etc/firmware/dsp2.mbn \
+     $(LOCAL_PATH)/etc/firmware/dsp3.mbn:system/etc/firmware/dsp3.mbn \
+     $(LOCAL_PATH)/etc/firmware/efs1.mbn:system/etc/firmware/efs1.mbn \
+     $(LOCAL_PATH)/etc/firmware/efs2.mbn:system/etc/firmware/efs2.mbn \
+     $(LOCAL_PATH)/etc/firmware/efs3.mbn:system/etc/firmware/efs3.mbn \
+     $(LOCAL_PATH)/etc/firmware/fw_bcmdhd.bin:system/etc/firmware/fw_bcmdhd.bin \
+     $(LOCAL_PATH)/etc/firmware/fw_bcmdhd_apsta.bin:system/etc/firmware/fw_bcmdhd_apsta.bin \
+     $(LOCAL_PATH)/etc/firmware/fw_bcmdhd_mfg.bin:system/etc/firmware/fw_bcmdhd_mfg.bin \
+     $(LOCAL_PATH)/etc/firmware/fw_bcmdhd_p2p.bin:system/etc/firmware/fw_bcmdhd_p2p.bin \
+     $(LOCAL_PATH)/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+     $(LOCAL_PATH)/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+     $(LOCAL_PATH)/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+     $(LOCAL_PATH)/etc/firmware/mdm_acdb.img:system/etc/firmware/mdm_acdb.img \
+     $(LOCAL_PATH)/etc/firmware/modem.b00:system/etc/firmware/modem.b00 \
+     $(LOCAL_PATH)/etc/firmware/modem.b01:system/etc/firmware/modem.b01 \
+     $(LOCAL_PATH)/etc/firmware/modem.b02:system/etc/firmware/modem.b02 \
+     $(LOCAL_PATH)/etc/firmware/modem.b03:system/etc/firmware/modem.b03 \
+     $(LOCAL_PATH)/etc/firmware/modem.b04:system/etc/firmware/modem.b04 \
+     $(LOCAL_PATH)/etc/firmware/modem.b05:system/etc/firmware/modem.b05 \
+     $(LOCAL_PATH)/etc/firmware/modem.b06:system/etc/firmware/modem.b06 \
+     $(LOCAL_PATH)/etc/firmware/modem.b07:system/etc/firmware/modem.b07 \
+     $(LOCAL_PATH)/etc/firmware/modem.b08:system/etc/firmware/modem.b08 \
+     $(LOCAL_PATH)/etc/firmware/modem.b09:system/etc/firmware/modem.b09 \
+     $(LOCAL_PATH)/etc/firmware/modem.b10:system/etc/firmware/modem.b10 \
+     $(LOCAL_PATH)/etc/firmware/modem.mdt:system/etc/firmware/modem.mdt \
+     $(LOCAL_PATH)/etc/firmware/modem_fw._:system/etc/firmware/modem_fw._ \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b00:system/etc/firmware/modem_fw.b00 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b00:system/etc/firmware/modem_fw.b00 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b01:system/etc/firmware/modem_fw.b01 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b02:system/etc/firmware/modem_fw.b02 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b03:system/etc/firmware/modem_fw.b03 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b04:system/etc/firmware/modem_fw.b04 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b05:system/etc/firmware/modem_fw.b05 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b06:system/etc/firmware/modem_fw.b06 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b07:system/etc/firmware/modem_fw.b07 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b08:system/etc/firmware/modem_fw.b08 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b09:system/etc/firmware/modem_fw.b09 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b10:system/etc/firmware/modem_fw.b10 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b11:system/etc/firmware/modem_fw.b11 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b12:system/etc/firmware/modem_fw.b12 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b13:system/etc/firmware/modem_fw.b13 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b14:system/etc/firmware/modem_fw.b14 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b15:system/etc/firmware/modem_fw.b15 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b16:system/etc/firmware/modem_fw.b16 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b23:system/etc/firmware/modem_fw.b23 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b24:system/etc/firmware/modem_fw.b24 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b25:system/etc/firmware/modem_fw.b25 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b27:system/etc/firmware/modem_fw.b27 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b28:system/etc/firmware/modem_fw.b28 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.b31:system/etc/firmware/modem_fw.b31 \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.fli:system/etc/firmware/modem_fw.fli \
+     $(LOCAL_PATH)/etc/firmware/modem_fw.mdt:system/etc/firmware/modem_fw.mdt \
+     $(LOCAL_PATH)/etc/firmware/q6.b00:system/etc/firmware/q6.b00 \
+     $(LOCAL_PATH)/etc/firmware/q6.b01:system/etc/firmware/q6.b01 \
+     $(LOCAL_PATH)/etc/firmware/q6.b03:system/etc/firmware/q6.b03 \
+     $(LOCAL_PATH)/etc/firmware/q6.b04:system/etc/firmware/q6.b04 \
+     $(LOCAL_PATH)/etc/firmware/q6.b05:system/etc/firmware/q6.b05 \
+     $(LOCAL_PATH)/etc/firmware/q6.b06:system/etc/firmware/q6.b06 \
+     $(LOCAL_PATH)/etc/firmware/q6.mdt:system/etc/firmware/q6.mdt \
+     $(LOCAL_PATH)/etc/firmware/rpm.mbn:system/etc/firmware/rpm.mbn \
+     $(LOCAL_PATH)/etc/firmware/sbl1.mbn:system/etc/firmware/sbl1.mbn \
+     $(LOCAL_PATH)/etc/firmware/sbl2.mbn:system/etc/firmware/sbl2.mbn \
+     $(LOCAL_PATH)/etc/firmware/tzapps.b00:system/etc/firmware/tzapps.b00 \
+     $(LOCAL_PATH)/etc/firmware/tzapps.b01:system/etc/firmware/tzapps.b01 \
+     $(LOCAL_PATH)/etc/firmware/tzapps.b02:system/etc/firmware/tzapps.b02 \
+     $(LOCAL_PATH)/etc/firmware/tzapps.b03:system/etc/firmware/tzapps.b03 \
+     $(LOCAL_PATH)/etc/firmware/tzapps.mdt:system/etc/firmware/tzapps.mdt \
+     $(LOCAL_PATH)/etc/firmware/vidc.b00:system/etc/firmware/vidc.b00 \
+     $(LOCAL_PATH)/etc/firmware/vidc.b01:system/etc/firmware/vidc.b01 \
+     $(LOCAL_PATH)/etc/firmware/vidc.b02:system/etc/firmware/vidc.b02 \
+     $(LOCAL_PATH)/etc/firmware/vidc.b03:system/etc/firmware/vidc.b03 \
+     $(LOCAL_PATH)/etc/firmware/vidc.mdt:system/etc/firmware/vidc.mdt \
+     $(LOCAL_PATH)/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
+     $(LOCAL_PATH)/etc/firmware/vidcfw.elf:system/etc/firmware/vidcfw.elf 
+
+# Widevine Permissions
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
+
+
+# snd_soc_msm
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/etc/snd_soc_msm/FM_A2DP_REC:system/etc/snd_soc_msm/FM_A2DP_REC \
+     $(LOCAL_PATH)/etc/snd_soc_msm/FM_A2DP_REC_2x:system/etc/snd_soc_msm/FM_A2DP_REC_2x \
+     $(LOCAL_PATH)/etc/snd_soc_msm/FM_Digital_Radio:system/etc/snd_soc_msm/FM_Digital_Radio \
+     $(LOCAL_PATH)/etc/snd_soc_msm/FM_Digital_Radio_2x:system/etc/snd_soc_msm/FM_Digital_Radio_2x \
+     $(LOCAL_PATH)/etc/snd_soc_msm/FM_REC:system/etc/snd_soc_msm/FM_REC \
+     $(LOCAL_PATH)/etc/snd_soc_msm/FM_REC_2x:system/etc/snd_soc_msm/FM_REC_2x \
+     $(LOCAL_PATH)/etc/snd_soc_msm/snd_soc_msm:system/etc/snd_soc_msm/snd_soc_msm \
+     $(LOCAL_PATH)/etc/snd_soc_msm/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
+     $(LOCAL_PATH)/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
+     $(LOCAL_PATH)/etc/snd_soc_msm/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar \
+     $(LOCAL_PATH)/etc/snd_soc_msm/snd_soc_msm_Taiko:system/etc/snd_soc_msm/snd_soc_msm_Taiko
+
+
+# Wifi
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+     $(LOCAL_PATH)/etc/wifi/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
+     $(LOCAL_PATH)/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf 
+ 
+
+
