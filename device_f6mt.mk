1c366521954be1afcd569d597066d8ec1a0901f4
dm47021 <dm47021@hotmail.com>
2014-11-01 04:07:30 -0400

    begin addind ramdisk

--- device_f6mt.mk
+++ device_f6mt.mk
@@ -1,32 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-
-
-# Audio
-PRODUCT_PACKAGES += \
-    alsa.msm8960 \
-    audio.a2dp.default \
-    audio_policy.msm8960 \
-    audio.primary.msm8960 \
-    libalsa-intf \
-    libaudioutils
-
-# Bluetooth
-
-# Graphics
-PRODUCT_PACKAGES += \
-    lights.msm8960 \
-    copybit.msm8960 \
-    gralloc.msm8960 \
-    hwcomposer.msm8960 \
-    libgenlock \
-    libmemalloc \
-    liboverlay \
-    libQcomUI \
-    libtilerenderer
 
 
 
