0996ef71bb66853464719b974fad42dee97697d8
dm47021 <dm47021@hotmail.com>
2014-11-01 06:21:55 -0400

    prebuilt-bin.mk

---
+++ prebuilt/prebuilt-bin.mk
@@ -0,0 +1,57 @@
+
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/bin/atd:system/bin/atd \
+     $(LOCAL_PATH)/bin/ATFWD-daemon:system/bin/ATFWD-daemon \
+     $(LOCAL_PATH)/bin/BCM4330B1_002.001.003.0967.1019.hcd:system/bin/BCM4330B1_002.001.003.0967.1019.hcd \
+     $(LOCAL_PATH)/bin/bridgemgrd:system/bin/bridgemgrd \
+     $(LOCAL_PATH)/bin/btld:system/bin/btld \
+     $(LOCAL_PATH)/bin/cnd:system/bin/cnd \
+     $(LOCAL_PATH)/bin/ds_fmc_appd:system/bin/ds_fmc_appd \
+     $(LOCAL_PATH)/bin/hostapd_cli:system/bin/hostapd_cli \
+     $(LOCAL_PATH)/bin/immvibed_f6_2v:system/bin/immvibed_f6_2v \
+     $(LOCAL_PATH)/bin/immvibed_f6_3v:system/bin/immvibed_f6_3v \
+     $(LOCAL_PATH)/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
+     $(LOCAL_PATH)/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+     $(LOCAL_PATH)/bin/mpdecision:system/bin/mpdecision \
+     $(LOCAL_PATH)/bin/netmgrd:system/bin/netmgrd \
+     $(LOCAL_PATH)/bin/nl_listener:system/bin/nl_listener \
+     $(LOCAL_PATH)/bin/port-bridge:system/bin/port-bridge \
+     $(LOCAL_PATH)/bin/qmiproxy:system/bin/qmiproxy \
+     $(LOCAL_PATH)/bin/qmuxd:system/bin/qmuxd \
+     $(LOCAL_PATH)/bin/qosmgr:system/bin/qosmgr \
+     $(LOCAL_PATH)/bin/qseecomd:system/bin/qseecomd \
+     $(LOCAL_PATH)/bin/radish:system/bin/radish \
+     $(LOCAL_PATH)/bin/rild:system/bin/rild \
+     $(LOCAL_PATH)/bin/rmt_storage:system/bin/rmt_storage \
+     $(LOCAL_PATH)/bin/sensord:system/bin/sensord \
+     $(LOCAL_PATH)/bin/sensors.qcom:system/bin/sensors.qcom \
+     $(LOCAL_PATH)/bin/thermald:system/bin/thermald \
+     $(LOCAL_PATH)/bin/time_daemon:system/bin/time_daemon \
+     $(LOCAL_PATH)/bin/usbhub:system/bin/usbhub \
+     $(LOCAL_PATH)/bin/usbhub_init:system/bin/usbhub_init \
+     $(LOCAL_PATH)/bin/v4l2-qcamera-app:system/bin/v4l2-qcamera-app
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+     
